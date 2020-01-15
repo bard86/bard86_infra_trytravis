aee5c2e28e5133364baa662fca25377aeb0fdd03
Denis Barsukov <bard86@list.ru>
2020-01-16 00:57:25 +0300

    add clone repo playbook

--- VPN/certbot.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install software-properties-common
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
-sudo apt-get --assume-yes update
-sudo apt-get --assume-yes install certbot
-sudo certbot certonly --standalone
